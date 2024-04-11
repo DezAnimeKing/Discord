5616a6c1aff0905381419fc36fea76f946fdf76a
DezAnimeKing <53986899+DezAnimeKing@users.noreply.github.com>
2024-04-11 01:51:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('mrnekrozyt')
     .setDetails(`.`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/828552178746654721/1227820598584021002/1227820522272854067remix-1712805641636.png?ex=6629cc0a&is=6617570a&hm=a4070e9c7f7dad6dd8d39b3d&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/828552178746654721/1227820598584021002/1227820522272854067remix-1712805641636.png?ex=6629cc0a&is=6617570a&hm=7b7daf5aa1eff7a840358c9550d2816b7a6002b6a4070e9c7f7dad6dd8d39b3d&') //You can put links in tenor or discord and etc.
 .setAssetsLargeText('.') //Text when you hover the Large image
 
 .setAssetsSmallText('loser') //Text when you hover the Small image 
