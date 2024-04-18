ad5c8d942c4c4415b09afa713f221612b58a5a58
DezAnimeKing <53986899+DezAnimeKing@users.noreply.github.com>
2024-04-18 01:28:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ client.on('ready', async () => {
     .setApplicationId('1188655662880784484')
     .setType('STREAMING')
     .setURL('https://www.twitch.tv/developer') //Must be a youtube video link 
-    .setState('│')
+    .setState('ㅤㅤ ㅤㅤ ㅤㅤ ㅤ ✗    ')
     .setName('mrnekrozyt')
-    .setDetails(`│`)
+    .setDetails(`ㅤㅤ ㅤtׁׅɑׁׅƙׁׅꫀׁׅܻ 𝕞𝕪 𝒃𝒓𝒆𝒂𝒕𝒉𝒆   .`⸃⸃ ㇀`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/828552178746654721/1227859209769713724/Picsart_24-04-11_01-53-48-163.jpg?ex=6629f000&is=66177b00&hm=8b388a5bc8b323d599154a3fd9b99fc6eafa0cdfdd237457993832a1569f2551&') //You can put links in tenor or discord and etc.
-.setAssetsLargeText('.') //Text when you hover the Large image
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/828552196114743296/1230382870049067058/200w_1.gif?ex=66331e57&is=6620a957&hm=074e82a884019bc4ad62cc0a02537b524427e47a3cd072358fa5b26c7fbc89e3&') //You can put links in tenor or discord and etc.
+.setAssetsLargeText('    ﹐﹐ ⸝⸝  Ａｗａｙ - ') //Text when you hover the Large image
 
 .setAssetsSmallText('loser') //Text when you hover the Small image 
     .addButton('⟬ 𝑋 ༚ 𝑂 ⟭', 'https://open.spotify.com/track/5ZicFGBDAi9J2YCVesboUp?si=KCmQHJnhSPG2uoFoblg_xQ')
